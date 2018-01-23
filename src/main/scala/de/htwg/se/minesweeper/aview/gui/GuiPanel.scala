4f667789d9fffd74d104cd68eab000c8f185ea53
Sebi1006 <32549631+Sebi1006@users.noreply.github.com>
2018-01-23 21:03:27 +0100

    Delete GuiPanel.scala

--- src/main/scala/de/htwg/se/minesweeper/aview/gui/GuiPanel.scala
+++
@@ -1,9 +0,0 @@
-package de.htwg.se.minesweeper.aview.gui
-
-import de.htwg.se.minesweeper.controller.Controller
-
-import scala.swing.Frame
-
-class GuiPanel(controller: Controller) extends Frame{
-
-}
