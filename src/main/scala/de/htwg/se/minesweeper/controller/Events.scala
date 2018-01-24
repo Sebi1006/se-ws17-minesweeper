ea37525fc1a34406d5d50502d1c5a225d19a2c1a
da721wol <32591918+da721wol@users.noreply.github.com>
2018-01-24 18:35:22 +0100

    Delete Events.scala

--- src/main/scala/de/htwg/se/minesweeper/controller/Events.scala
+++
@@ -1,7 +0,0 @@
-package de.htwg.se.minesweeper.controller
-
-import scala.swing.event.Event
-
-case class CellChanged() extends Event
-case class GridSizeChanged(height: Int, width: Int, mineNumber: Int) extends Event
-case class Winner(win: Boolean) extends Event
