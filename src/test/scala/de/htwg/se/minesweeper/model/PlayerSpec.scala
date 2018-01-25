ffaa27326e05c2a5eacf9e0ce0a912bf4b9f49e6
Sebi1006 <32549631+Sebi1006@users.noreply.github.com>
2018-01-25 01:06:18 +0100

    Updated PlayerSpec

--- src/test/scala/de/htwg/se/minesweeper/model/PlayerSpec.scala
+++ src/test/scala/de/htwg/se/minesweeper/model/PlayerSpec.scala
@@ -1,5 +1,6 @@
 package de.htwg.se.minesweeper.model
 
+import de.htwg.se.minesweeper.model.playerComponent.Player
 import org.scalatest._
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
